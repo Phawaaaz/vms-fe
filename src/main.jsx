35fe6f799250b00c4c52c5387cb936ce66517afe
Trayshmhirk <harlex.mikkey@gmail.com>
2025-05-02 14:59:39 +0100

    discard app.css

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "./routes/routes";
 import "./index.css";
-import "./App.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
